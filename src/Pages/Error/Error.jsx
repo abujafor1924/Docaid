ba64d4fbad255d3caba5bd7cd82fbe2ed65e0a0b
abu-jafor <abujaforwd@yahoo.com>
2023-06-19 11:19:49 +0600

    Error Page Added

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div className=" mx-auto">
-      <h1>Error 404?</h1>
-      <Link to={"/"}>
-        <button>Back Home</button>
+    <div className=" w-[900px] mx-auto  relative ">
+      <img src="https://i.ibb.co/QJNgYvL/error-404.jpg" alt="Error" />
+      <Link to={"/"} className="absolute bottom-5 left-[390px]">
+        <button className="btn btn-error text-white ">Back Home</button>
       </Link>
     </div>
   );
